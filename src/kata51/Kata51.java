df9067f4a756917ebf6c645aeb4c51a775e62688
tanay <tanay@DESKTOP-J2I6D30>
2023-01-04 13:06:13 +0000

    V1 P1

---
+++ src/kata51/Kata51.java
@@ -0,0 +1,46 @@
+package kata51;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class Kata51 {
+    public static void main(String[] args) {
+        Connection conexion=connect("Kata5.db");
+        selectfromdb("SELECT * FROM PEOPLE",conexion);
+        /*String sql = """ para la v2
+                     CREATE TABLE IF NOT EXISTS direcc_email (
+                      id integer PRIMARY KEY AUTOINCREMENT,
+                      direccion text NOT NULL);""";*/
+        //createtable(sql,conexion);
+    }
+    private static Connection connect(String bdurl) {
+        String url = "jdbc:sqlite:"+bdurl;
+        Connection conn = null;
+        try {
+        conn = DriverManager.getConnection(url);
+        } 
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    public static void selectfromdb(String select,Connection conexion){
+        try (
+        Statement stmt = conexion.createStatement();
+        ResultSet rs = stmt.executeQuery(select))
+        {
+            // Bucle sobre el conjunto de registros.
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                rs.getString("Name") + "\t" +
+                rs.getString("Apellidos") + "\t" +
+                rs.getString("Departamento") + "\t");
+            }
+        } 
+        catch (SQLException e) {
+        System.out.println(e.getMessage());
+        }
+    }
+}
+
